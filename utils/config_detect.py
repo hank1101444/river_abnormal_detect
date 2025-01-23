2fbd614cab934b7b93e7a2c79f1c6a66d4061d05
ac_orz <94980195+hank1101444@users.noreply.github.com>
2025-01-23 14:22:00 +0800

    Update config_detect.py

--- utils/config_detect.py
+++ utils/config_detect.py
@@ -6,8 +6,8 @@ image_save_path = None
 start_frame = None
 end_frame = None    
 ### init line_bot_api
-line_bot_api = LineBotApi("++44xjAqn8haqrc/HDcUcy6KWpW1ihsFoyZ1WGuXpJ6x1gb11cV3U88ye/msw8SYnfW/8WqBhQlxMGd87NGz5L2rGFiXkD09lFChMYivH8oH/5SpBJZ0YyWXIxRCS1zHyTHc3Z6l2SmFbjVdJgpDgAdB04t89/1O/w1cDnyilFU=")  # 將 "YOUR_CHANNEL_ACCESS_TOKEN" 替換為你的 Channel Access Token
-user_id = "U5afff4c1c5dbc6cdb488c9e7bc5eb585"  # Line ID
-tg_token = "7455340197:AAGj4x9GYpdEcJWlAu7_G6VeXMbEK6qAlp0" #tg bot token
-tg_chat_id = "-1002485671954" #tg chat id
-# https://api.telegram.org/bot7978169585:AAEPwKpapon1sAbOXuDvY7MSa3_HagJGdqU/getUpdates
+line_bot_api = LineBotApi("YOUR_LINE_BOT_API")  # 將 "YOUR_CHANNEL_ACCESS_TOKEN" 替換為你的 Channel Access Token
+user_id = "YOUR_LINE_ID"  # Line ID
+tg_token = "YOUR_TG_BOT_TOKEN" #tg bot token
+tg_chat_id = "YOUR_TG_CHAT_ID" #tg chat id
+# https://api.telegram.org/bot{tg_token}/getUpdates
